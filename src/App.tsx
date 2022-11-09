6ee93f545b48c0b27cd9e56088ac6c14e41cb95f
khajimatov <for.official.bkh@gmail.com>
2022-11-09 20:13:18 +0600

    Added ColorCard component Deleted unnecessary css code

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
-import { useState } from 'react'
+import React from 'react'
 import './App.css'
+import ColorCard from './components/ColorCard';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
+      <ColorCard />
     </div>
   )
 }
 
-export default App
+export default App;
---
+++ src/components/ColorCard/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './ColorCard.module.scss';
+
+const ColorCard = () => {
+    return (
+        <div className={styles.colorCard}></div>
+    )
+}
+
+export default ColorCard;
