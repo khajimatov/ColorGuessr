68ca6c81e054336363e58fe145e01da9b80fe3ae
khajimatov <for.official.bkh@gmail.com>
2022-11-10 11:27:56 +0600

    Changed emoji

--- src/components/ColorCard/index.tsx
+++ src/components/ColorCard/index.tsx
@@ -4,7 +4,7 @@ import styles from './ColorCard.module.scss';
 const ColorCard = () => {
     return (
         <div className={styles.colorCard}>
-            <p>🤔</p>
+            <p>🧐</p>
         </div>
     )
 }
